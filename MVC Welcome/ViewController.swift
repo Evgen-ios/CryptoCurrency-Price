5033ad9b77e60d2f44e14714c0950542d517e717
Evgen-ios <jack38irk@icloud.com>
2021-08-18 10:38:47 +0800

    Initial Commit

---
+++ MVC Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC Welcome
+//
+//  Created by Evgeniy Goncharov on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
